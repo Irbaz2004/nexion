4ba703dd7d7c6d7712e8ec73297b6a498acd7c81
Irbaz2004 <irbazahmeds20@gmail.com>
2024-07-07 14:36:30 +0530

    first

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ window.onload = function(){
 }
 
 
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_cxk6ryf';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
